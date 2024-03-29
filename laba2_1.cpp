86242c2c0f1a0a45df74f973aad33674e2ae114b
LudovDI <70688004+LudovDI@users.noreply.github.com>
2024-03-29 20:23:42 +0300

    new version

--- laba2_1.cpp
+++ laba2_1.cpp
@@ -15,10 +15,10 @@
 #define PORT 6789
 #define BUFFER_SIZE 1024
 
-void handle_client(SOCKET clientSocket) {
+void handle_client(SOCKET my_client_socket) {
     try {
         char buffer[BUFFER_SIZE];
-        int bytes_received = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+        int bytes_received = recv(my_client_socket, buffer, BUFFER_SIZE, 0);
         if (bytes_received == SOCKET_ERROR) {
             throw "Error receiving data from client";
         }
@@ -30,51 +30,51 @@ void handle_client(SOCKET clientSocket) {
 
         if (!file.is_open()) {
             std::string not_found_response = "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found";
-            send(clientSocket, not_found_response.c_str(), not_found_response.size(), 0);
+            send(my_client_socket, not_found_response.c_str(), not_found_response.size(), 0);
         }
         else {
             std::string response_header = "HTTP/1.1 200 OK\r\n\r\n";
-            send(clientSocket, response_header.c_str(), response_header.size(), 0);
+            send(my_client_socket, response_header.c_str(), response_header.size(), 0);
             std::string line;
             while (std::getline(file, line)) {
-                send(clientSocket, line.c_str(), line.size(), 0);
+                send(my_client_socket, line.c_str(), line.size(), 0);
             }
             file.close();
         }
 
-        closesocket(clientSocket);
+        closesocket(my_client_socket);
     }
     catch (...) {
-        closesocket(clientSocket);
+        closesocket(my_client_socket);
     }
 }
 
 int main() {
-    WSADATA wsaData;
-    SOCKET serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    int sin_size = sizeof(clientAddr);
+    WSADATA wsa_data;
+    SOCKET server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    int sin_size = sizeof(client_addr);
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
         std::cerr << "WSAStartup failed" << std::endl;
         return 1;
     }
 
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
         std::cerr << "Error creating socket" << std::endl;
         return 1;
     }
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         std::cerr << "Error binding to socket" << std::endl;
         return 1;
     }
 
-    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+    if (listen(server_socket, 5) == SOCKET_ERROR) {
         std::cerr << "Error listening on socket" << std::endl;
         return 1;
     }
@@ -82,18 +82,18 @@ int main() {
     std::cout << "Server is ready to accept connections." << std::endl;
 
     while (true) {
-        if ((clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &sin_size)) == INVALID_SOCKET) {
+        if ((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &sin_size)) == INVALID_SOCKET) {
             std::cerr << "Error accepting connection" << std::endl;
             continue;
         }
 
-        std::cout << "Connection established with " << inet_ntoa(clientAddr.sin_addr) << std::endl;
+        std::cout << "Connection established with " << inet_ntoa(client_addr.sin_addr) << std::endl;
 
-        std::thread client_thread(handle_client, clientSocket);
+        std::thread client_thread(handle_client, client_socket);
         client_thread.detach();
     }
 
-    closesocket(serverSocket);
+    closesocket(server_socket);
     WSACleanup();
     return 0;
 }
